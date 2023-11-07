b5779c6fc1b040448e266e3d8343aeb099356ead
ArtemisFowl24 <tahers7@mcmaster.ca>
2023-11-07 01:40:00 -0500

    Create simplex.py
    Created and optimised

---
+++ simplex.py
@@ -0,0 +1,138 @@
+import copy
+import fractions 
+
+# Takes a matrix(list of lists) and performs the row operation r1 -> r1*xr1 'op' r2*yr2
+# Example: taboper([[1,2,3],[4,5,6],[7,8,9]],1,2,3,4,"+") (Row 1 -> Row 1*3 + Row 2*4)
+# Returns: [[13, 14, 15], [4, 5, 6], [7, 8, 9]]
+def taboper(l,r1,r2,xr1,yr2,op):
+    r1=r1-1
+    r2=r2-1
+    l1=copy.deepcopy(l)
+    if op=="*":
+        for pos,i in enumerate(l1[r1]):
+            l1[r1][pos] = l1[r1][pos] * xr1
+            if type(l1[r1][pos])==fractions.Fraction:
+                l1[r1][pos]=l1[r1][pos].numerator
+        return l1
+    if op=="/":
+        for pos,i in enumerate(l1[r1]):
+            if l1[r1][pos] % xr1 == 0:
+                l1[r1][pos] = l1[r1][pos] // xr1
+            else:
+                l1[r1][pos] = fractions.Fraction(l1[r1][pos],xr1)
+        return l1
+    newr1=[]
+    newr2=[]
+    for i in l1[r1]:
+        newr1.append(i*xr1)
+    for i in l1[r2]:
+        newr2.append(i*yr2)
+    if op=="+":
+        for pos,i in enumerate(l1[r1]):
+            if type(newr1[pos])==int and type(newr2[pos])==int:
+                l1[r1][pos]=newr1[pos]+newr2[pos]
+            else:
+                l1[r1][pos]=fractions.Fraction(newr1[pos]+newr2[pos])
+                if l1[r1][pos].denominator == 1:
+                    l1[r1][pos]=l1[r1][pos].numerator
+                if l1[r1][pos].numerator == 0:
+                    l1[r1][pos]=0
+        return l1
+    else:
+        for pos,i in enumerate(l1[r1]):
+            if type(newr1[pos])==int and type(newr2[pos])==int:
+                l1[r1][pos]=newr1[pos]-newr2[pos]
+            else:
+                l1[r1][pos]=fractions.Fraction(newr1[pos]-newr2[pos])
+                if l1[r1][pos].denominator == 1:
+                    l1[r1][pos]=l1[r1][pos].numerator
+                if l1[r1][pos].numerator == 0:
+                    l1[r1][pos]=0
+        return l1
+
+# Takes a matrix(list of lists) and returns the matrix after reducing around a pivot found using Danzig's method
+def pivot(l):
+    max = 0
+    er=0
+    for pos in range(0,len(l[0])-1):
+            if l[0][pos]>=max:
+                max = l[0][pos]
+                er = pos
+    if max == 0:
+        return l
+    col=[]
+    column=0
+    for i in range(0,len(l)):
+        print(l[i][er])
+        col.append(l[i][er])
+    min = 100000
+    for i in range(1,len(col)):
+        if col[i] != 0:
+            if (l[i][-1]/col[i]) < min and (l[i][-1]/col[i]) >= 0:
+                min = l[i][-1]/col[i]
+                column = i
+    if min == 100000:
+        return l
+    l2=copy.deepcopy(l)
+    low=l2[column][er]
+    for k in range(0,len(l2[column])):
+        if low == 0:
+            break
+        if l2[column][k]%low == 0: 
+            l2[column][k] = l2[column][k]//low
+        else:
+            l2[column][k] = fractions.Fraction(l2[column][k],low)
+    for i in range(0,len(l2)):
+        value=l2[i][er]
+        for pos in range(0,len(l2[i])):
+            if i!=column:
+                l2[i][pos] = l2[i][pos] - value*l2[column][pos]
+    return l2
+
+# Just takes a string and returns a list of numbers inc fractions
+# Format the string like this: "1 2 3/4 5 6 7/8"
+def commarize(n):
+    for i in n:
+        if i==" ":
+            n=n.replace(i,",")
+    n=n.split(",")
+    for pos,i in enumerate(n):
+        if "/" not in i:
+            n[pos]=int(i)
+        else:
+            n[pos]=fractions.Fraction(int(i.split("/")[0]),int(i.split("/")[1]))
+    return n
+
+# Pretty prints a matrix(list of lists) row by row
+def prettyprint(l):
+    l2=copy.deepcopy(l)
+    for pos,i in enumerate(l2):
+            for posi,j in enumerate(i):
+                if len(str(j))==1:
+                    l2[pos][posi] = " " + str(l2[pos][posi])
+                else:
+                    l2[pos][posi] = str(l2[pos][posi])
+            print(i,end="\n")
+
+# Generates latex code given a matrix(list of lists)
+def generator(l):
+    for i in l:
+        for posi,j in enumerate(i):
+                if type(j) == int and posi == len(i)-1:
+                    print(j,end=" \\\ \\hline \n")
+                elif type(j) != int and posi == len(i)-1:
+                    if j.denominator == 1:
+                        print(j.numerator,end=" \\\ \\hline \n")
+                    elif j.numerator == 0:
+                        print(0,end=" \\\ \\hline \n")
+                    else:
+                        print("$\frac{"+str(j.numerator)+"}{"+str(j.denominator)+"}$",end=" \\\ \\hline \n")
+                elif type(j) == int:
+                    print(j,end=" & ")
+                else:
+                    if j.denominator == 1:
+                        print(j.numerator,end=" & ")
+                    elif j.numerator == 0:
+                        print(0,end=" & ")
+                    else:
+                        print("$\\frac{"+str(j.numerator)+"}{"+str(j.denominator)+"}$",end=" & ")
